5da543ab6c041dcb6277fb8156f75ca3c3e5615a
Mashrafi Rahman <monomycelium@icloud.com>
2022-05-29 23:22:28 +0800

    Fix a Bug
    There was a bug where LTA showcases its obstinacy with such flair. I also added an interactive snippet of code.

--- bus.py
+++ bus.py
@@ -2,22 +2,25 @@ import requests
 from datetime import datetime
 import json
 
-url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=43389"
-payload={}
 headers = {
   'AccountKey': 'YgpFVWKSQzatgONxbRGkyQ==',
   'accept\'': 'application/json'
 }
-response = requests.request("GET", url, headers=headers, data=payload)
-dataX = json.loads(response.text)['Services'][2]
+response = requests.request("GET", "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=%s" % input('Enter Bus Stop Number: '), headers=headers, data={})
+dataX = json.loads(response.text)['Services'][int(input('Enter Bus Number: '))]
 
-timeX = datetime.strptime(dataX['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
-timeY = datetime.strptime(dataX['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
-if len(str(dataX['NextBus3']['EstimatedArrival'])) == len(str(dataX['NextBus2']['EstimatedArrival'])):
+print(dataX['ServiceNo'])
+
+if len(str(dataX['NextBus2']['EstimatedArrival'])) == 25:
+    timeX = datetime.strptime(dataX['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+if len(str(dataX['NextBus2']['EstimatedArrival'])) == 25:
+    timeY = datetime.strptime(dataX['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+if len(str(dataX['NextBus3']['EstimatedArrival'])) == 25:
     timeZ = datetime.strptime(dataX['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
 
-if len(str(timeX)) == len(str(timeY)):
+if len(str(timeX)) == 14:
     print(str(timeX)[2:7])
-print(str(timeY)[2:7])
-if len(str(dataX['NextBus3']['EstimatedArrival'])) == len(str(dataX['NextBus2']['EstimatedArrival'])):
+if len(str(timeY)) == 14:
+    print(str(timeY)[2:7])
+if len(str(timeZ)) == 14:
     print(str(timeZ)[2:7])
