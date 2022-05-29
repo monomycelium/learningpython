b89b2cd1e2ecf192fc9baea6c32cdca873e1311a
Mashrafi Rahman <monomycelium@icloud.com>
2022-05-29 22:21:23 +0800

    Fixed an API Bug
    I fixed a bug where LTA would not send me `timeZ`, which would result in an error.

--- bus.py
+++ bus.py
@@ -13,9 +13,11 @@ dataX = json.loads(response.text)['Services'][2]
 
 timeX = datetime.strptime(dataX['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
 timeY = datetime.strptime(dataX['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
-timeZ = datetime.strptime(dataX['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+if len(str(dataX['NextBus3']['EstimatedArrival'])) == len(str(dataX['NextBus2']['EstimatedArrival'])):
+    timeZ = datetime.strptime(dataX['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
 
 if len(str(timeX)) == len(str(timeY)):
     print(str(timeX)[2:7])
 print(str(timeY)[2:7])
-print(str(timeZ)[2:7])
+if len(str(dataX['NextBus3']['EstimatedArrival'])) == len(str(dataX['NextBus2']['EstimatedArrival'])):
+    print(str(timeZ)[2:7])
