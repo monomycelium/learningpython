194c2c5787103b95e4f797314e6c3d3427a6deec
Mashrafi Rahman <monomycelium@icloud.com>
2022-05-29 22:02:11 +0800

    Improve the Code
    Fixed a performance bug.

---
+++ bus.py
@@ -0,0 +1,21 @@
+import requests
+from datetime import datetime
+import json
+
+url = "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=43389"
+payload={}
+headers = {
+  'AccountKey': 'YgpFVWKSQzatgONxbRGkyQ==',
+  'accept\'': 'application/json'
+}
+response = requests.request("GET", url, headers=headers, data=payload)
+dataX = json.loads(response.text)['Services'][2]
+
+timeX = datetime.strptime(dataX['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+timeY = datetime.strptime(dataX['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+timeZ = datetime.strptime(dataX['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+
+if len(str(timeX)) == len(str(timeY)):
+    print(str(timeX)[2:7])
+print(str(timeY)[2:7])
+print(str(timeZ)[2:7])
