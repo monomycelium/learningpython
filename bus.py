0f1c203fc3b401e0fb3d5c6565061af8316ef3ef
Mashrafi Rahman <monomycelium@icloud.com>
2022-05-30 21:36:39 +0800

    Improved Code and Fixed Bugs
    I made it closer than ever to get the new data from the Raspberry Pi and fixed some bugs.

--- bus.py
+++ bus.py
@@ -11,10 +11,16 @@ dataX = json.loads(response.text)['Services'][2]
 
 if len(str(dataX['NextBus2']['EstimatedArrival'])) == 25:
     timeX = datetime.strptime(dataX['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+else:
+    timeX = None
 if len(str(dataX['NextBus2']['EstimatedArrival'])) == 25:
     timeY = datetime.strptime(dataX['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+else:
+    timeY = None
 if len(str(dataX['NextBus3']['EstimatedArrival'])) == 25:
     timeZ = datetime.strptime(dataX['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+else:
+    timeZ = None
 
 if len(str(timeX)) == 14:
     print(str(timeX)[2:7])
