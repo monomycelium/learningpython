6de095c5ed14484af507f5cc7fec0f33b69cb2ca
Mashrafi Rahman <monomycelium@icloud.com>
2022-07-10 11:12:59 +0800

    Simplify scripts

--- off.py
+++ off.py
@@ -35,63 +35,63 @@ import smbus
 import time
 
 I2C_ADDR  = 0x3F # I2C address
-LCD_WIDTH = 16
+# LCD_WIDTH = 16
 
-LCD_CHR = 1
-LCD_CMD = 0
+# LCD_CHR = 1
+# LCD_CMD = 0
 
-LCD_LINE_1 = 0x80
-LCD_LINE_2 = 0xC0
-LCD_LINE_3 = 0x94
-LCD_LINE_4 = 0xD4
+# LCD_LINE_1 = 0x80
+# LCD_LINE_2 = 0xC0
+# LCD_LINE_3 = 0x94
+# LCD_LINE_4 = 0xD4
 
-LCD_BACKLIGHT  = 0x08  # On
+# LCD_BACKLIGHT  = 0x08  # On
 # LCD_BACKLIGHT = 0x00  # Off
 
-ENABLE = 0b00000100
+# ENABLE = 0b00000100
 
-E_PULSE = 0.0005
-E_DELAY = 0.0005
+# E_PULSE = 0.0005
+# E_DELAY = 0.0005
 
-#bus = smbus.SMBus(0)  # Rev 1 Pi
-bus = smbus.SMBus(1) # Rev 2 Pi
+# #bus = smbus.SMBus(0)  # Rev 1 Pi
+# bus = smbus.SMBus(1) # Rev 2 Pi
 
-def lcd_init():
-  lcd_byte(0x33,LCD_CMD)
-  lcd_byte(0x32,LCD_CMD)
-  lcd_byte(0x06,LCD_CMD)
-  lcd_byte(0x0C,LCD_CMD)
-  lcd_byte(0x28,LCD_CMD)
-  lcd_byte(0x01,LCD_CMD)
-  time.sleep(E_DELAY)
+# def lcd_init():
+#   lcd_byte(0x33,LCD_CMD)
+#   lcd_byte(0x32,LCD_CMD)
+#   lcd_byte(0x06,LCD_CMD)
+#   lcd_byte(0x0C,LCD_CMD)
+#   lcd_byte(0x28,LCD_CMD)
+#   lcd_byte(0x01,LCD_CMD)
+#   time.sleep(E_DELAY)
 
-def lcd_byte(bits, mode):
+# def lcd_byte(bits, mode):
 
-  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+#   bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+#   bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
 
-  bus.write_byte(I2C_ADDR, bits_high)
-  lcd_toggle_enable(bits_high)
+#   bus.write_byte(I2C_ADDR, bits_high)
+#   lcd_toggle_enable(bits_high)
 
-  bus.write_byte(I2C_ADDR, bits_low)
-  lcd_toggle_enable(bits_low)
+#   bus.write_byte(I2C_ADDR, bits_low)
+#   lcd_toggle_enable(bits_low)
 
-def lcd_toggle_enable(bits):
-  time.sleep(E_DELAY)
-  bus.write_byte(I2C_ADDR, (bits | ENABLE))
-  time.sleep(E_PULSE)
-  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
-  time.sleep(E_DELAY)
+# def lcd_toggle_enable(bits):
+#   time.sleep(E_DELAY)
+#   bus.write_byte(I2C_ADDR, (bits | ENABLE))
+#   time.sleep(E_PULSE)
+#   bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
+#   time.sleep(E_DELAY)
 
-def main():
-  # Main program block
+# def main():
+#   # Main program block
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-  try:
-    lcd_init()
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    lcd_byte(0x01, LCD_CMD)
+#   try:
+#     lcd_init()
+#     main()
+#   except KeyboardInterrupt:
+#     pass
+#   finally:
+#     lcd_byte(0x01, LCD_CMD)
