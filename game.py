b3c23519a011e1755a515800ecd6ee1c5e2e5917
mashrafi <monomycelium@icloud.com>
2023-01-18 12:51:07 +0800

    finish lists.

---
+++ game.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+from tkinter import *
+import random
+import time
+class Ball:
+    
+    # We alter this a bit:
+    def __init__(self, canvas, paddle, score, color):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.score = score
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        self.x = starts[random.randrange(5)]
+        self.y = -2
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+    def hit_paddle(self, pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2] and pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+            self.x += self.paddle.x
+            
+            # Add this:
+            self.score.hit()
+            
+            return True
+        else:
+            return False
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 2
+        if pos[3] >= self.canvas_height:
+            self.y = -2
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            self.y = -2
+        if pos[0] <= 0:
+            self.x = 2
+        if pos[2] >= self.canvas_width:
+            self.x = -2
+class Paddle:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
+        self.canvas.move(self.id, 200, 300)
+        self.x = 0
+        self.canvas_width = self.canvas.winfo_width()
+        self.started = False
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        self.canvas.bind_all('<Button-1>', self.start_game)
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0    
+    def turn_left(self, evt):
+        self.x = -1
+    def turn_right(self, evt):
+        self.x = 1
+    def start_game(self, evt):
+        self.started = True
+        
+# We add this:
+class Score:
+    def __init__(self, canvas, color):
+        self.score = 0
+        self.canvas = canvas
+        self.id = canvas.create_text(450, 10, text=self.score, fill=color)
+    def hit(self):
+        self.score += 1
+        self.canvas.itemconfig(self.id, text = self.score)
+
+tk = Tk()
+tk.title('Game')
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+# Add this line:
+score = Score(canvas, 'green')
+paddle = Paddle(canvas, 'blue')
+
+# Alter this:
+ball = Ball(canvas, paddle, score, 'red')
+
+game_over_text = canvas.create_text(250, 200, text='GAME OVER', state='hidden')
+while 1:
+    while ball.hit_bottom == False and paddle.started == True:
+        ball.draw()
+        paddle.draw()
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.0083333333333333)
+    if paddle.started == False:
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(5)
+    else:
+        time.sleep(1)
+        canvas.itemconfig(game_over_text, state='normal')
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(5)
