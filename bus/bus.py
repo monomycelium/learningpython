bcc421509a1ea4528e0266662b0bb3ebf568bc76
mashrafi rahman <monomycelium@icloud.com>
2023-01-27 12:43:51 +0800

    improve `bus.py` and continue relearning built-ins.

--- bus/bus.py
+++ bus/bus.py
@@ -1,25 +1,45 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import requests
 from datetime import datetime
 import json
+import sys
 
-headers = {
-  'AccountKey': 'YgpFVWKSQzatgONxbRGkyQ==',
-  'accept\'': 'application/json'
-}
-response = requests.request("GET", "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=43389", headers=headers, data={})
-dataX = json.loads(response.text)['Services'][2]
+def fetch_data(bus_stop):
+    return(json.loads(requests.request("GET", "http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=%s" % bus_stop, headers={ 'AccountKey': 'YgpFVWKSQzatgONxbRGkyQ==' }, data={}).text)["Services"])
 
-if len(str(dataX['NextBus2']['EstimatedArrival'])) == 25:
-    timeX = datetime.strptime(dataX['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+def extract_map(data, bus_service):
+    for x in data:
+        if x["ServiceNo"] == bus_service:
+            break
+
+    if x["ServiceNo"] != bus_service:
+        print("bus service `%s` not found in bus stop `%s`. here are other buses that stop there: " % (bus_service, bus_stop))
+        list_services(data)
+        sys.exit(1)
+
+def list_services(data):
+    for x in data:
+        print(x["ServiceNo"])
+
+bus_stop = "43389"
+bus_service = "189"
+
+data = fetch_data(bus_stop)
+
+extract_map(data, bus_service)
+
+print(x['ServiceNo'])
+
+if len(str(x['NextBus']['EstimatedArrival'])) == 25:
+    timeX = datetime.strptime(x['NextBus']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
 else:
     timeX = None
-if len(str(dataX['NextBus2']['EstimatedArrival'])) == 25:
-    timeY = datetime.strptime(dataX['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+if len(str(x['NextBus2']['EstimatedArrival'])) == 25:
+    timeY = datetime.strptime(x['NextBus2']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
 else:
     timeY = None
-if len(str(dataX['NextBus3']['EstimatedArrival'])) == 25:
-    timeZ = datetime.strptime(dataX['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
+if len(str(x['NextBus3']['EstimatedArrival'])) == 25:
+    timeZ = datetime.strptime(x['NextBus3']['EstimatedArrival'], "%Y-%m-%dT%H:%M:%S+08:00") - datetime.now()
 else:
     timeZ = None
 
@@ -29,4 +49,3 @@ if len(str(timeY)) == 14:
     print(str(timeY)[2:7])
 if len(str(timeZ)) == 14:
     print(str(timeZ)[2:7])
-    
